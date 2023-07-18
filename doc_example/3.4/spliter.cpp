f29f801d26e2a7d17a3b3647e329b611361fdaf7
Weixiao Zhan <zhanweixiao@yeez.tech>
2023-07-18 16:20:28 +0800

    swap to netio functor

--- doc_example/3.4/spliter.cpp
+++ doc_example/3.4/spliter.cpp
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "hpda/processor/transform/split.h"
+#include "netio.cpp"
 
 // starting with a csv file reader
 class cvs_extractor : public hpda::extractor::internal::raw_data_impl<NTO_data_entry>
 {
 public:
     cvs_extractor(const std::string &filename)
-        :hpda::extractor::internal::raw_data_impl<NTO_data_entry>()
+        : hpda::extractor::internal::raw_data_impl<NTO_data_entry>()
     {
         file.open(filename);
         if (!file.is_open())
@@ -19,7 +20,8 @@ public:
         file.close();
     }
 
-    bool process() override {
+    bool process() override
+    {
         std::string line;
         if (std::getline(file, line))
         {
@@ -34,7 +36,7 @@ public:
 
             NTO_loc_info one_loc_info;
             one_loc_info.set<longitude, latitude, timestamp>(
-                std::stof(fields[1]), std::stof(fields[2]), std::stof(fields[3])); 
+                std::stof(fields[1]), std::stof(fields[2]), std::stof(fields[3]));
 
             NTO_data_entry one_entry;
             one_entry.set<phone_number>(std::stoll(fields[0]));
@@ -48,23 +50,23 @@ private:
     std::ifstream file;
 };
 
-
 class hash_spliter : public hpda::processor::internal::split_impl<NTO_data_entry>
 {
 public:
     hash_spliter(hpda::internal::processor_with_output<NTO_data_entry> *upper_stream)
         : hpda::processor::internal::split_impl<NTO_data_entry>(upper_stream)
-    {    }
+    {
+    }
 
     bool process() override
     {
-        if (m_streams.empty() || !base::has_input_value() )
+        if (m_streams.empty() || !base::has_input_value())
         {
             return false;
         }
         NTO_data_entry t = base::input_value();
         size_t hashValue = hashFunc(t.get<phone_number>());
-        m_streams[hashValue%m_streams.size()]->add_data(t.make_copy());
+        m_streams[hashValue % m_streams.size()]->add_data(t.make_copy());
         consume_input_value();
         return true;
     }
@@ -72,85 +74,6 @@ public:
 protected:
     std::hash<int> hashFunc;
 };
-
-//class output_client: 
-class output_client 
-    : public hpda::output::internal::memory_output_impl<NTO_data_entry>, 
-      public ff::net::routine
-{
-public:
-    output_client(hpda::internal::processor_with_output<NTO_data_entry> *upper_stream, int port) 
-        : hpda::output::internal::memory_output_impl<NTO_data_entry>(upper_stream),
-          ff::net::routine("spliter.output_client"),
-          port(port)
-    {   }
-
-    virtual void initialize(ff::net::net_mode nm, const std::vector<std::string> &args)
-    {
-        pkghub.tcp_to_recv_pkg<NTP_no_more_data_flag>(std::bind(&output_client::on_recv_no_data, this,
-                                    std::placeholders::_1,
-                                    std::placeholders::_2));
-        netn = new ff::net::net_nervure(nm);
-        netn->add_pkg_hub(pkghub);
-        netn->add_tcp_client("127.0.0.1", port);
-        netn->get_event_handler()->listen<ff::net::event::tcp_get_connection>(
-            std::bind(&output_client::on_conn_succ, this, std::placeholders::_1));
-        netn->get_event_handler()->listen<ff::net::event::tcp_lost_connection>(
-            std::bind(&output_client::on_conn_lost, this, std::placeholders::_1));
-    }
-
-    virtual void run()
-    {
-        netn->run();
-    }
-private:
-    void send_data(ff::net::tcp_connection_base *server) {
-        for (NTO_data_entry data : m_data) {
-            std::shared_ptr<NTP_data_entry> pkg = std::make_shared<NTP_data_entry>();
-            (*pkg).set<data_entry>(data);
-            server->send(pkg);
-            std::cout<< "sent data [" << data.get<phone_number>() << "]" << std::endl;
-        }
-        
-        std::shared_ptr<NTP_no_more_data_flag> pkg(new NTP_no_more_data_flag());
-        server->send(pkg);
-        std::cout<< "sent end flag" << std::endl;
-    }
-
-    void on_recv_no_data(std::shared_ptr<NTP_no_more_data_flag> msg,
-                    ff::net::tcp_connection_base *server)
-    {
-        done_transfer.store(true);
-    }
-
-    void on_conn_succ(ff::net::tcp_connection_base *server)
-    {
-        ff::net::mout << "connect success, sending data ..." << std::endl;
-        send_data(server);
-    }
-
-    void on_conn_lost(ff::net::tcp_connection_base *server)
-    {
-        ff::net::mout << "lost connection!" << std::endl;
-    }
-
-    void done_transfer_and_close_conn() {
-        while(!done_transfer.load()) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-        // done transfer;
-        netn->stop();
-    } 
-
-
-protected:
-    std::atomic<bool> done_transfer;
-
-    int port;
-    ff::net::typed_pkg_hub pkghub;
-    ff::net::net_nervure *netn;
-};
-
 
 int main(int argc, char *argv[])
 {
@@ -166,7 +89,6 @@ int main(int argc, char *argv[])
         std::cout << v.get<phone_number>() << std::endl;
     }
     */
-
 
     hash_spliter hs(&ce);
     hs.set_engine(&engine);
@@ -187,20 +109,10 @@ int main(int argc, char *argv[])
     }
     */
 
-    ff::net::application app("spliter");
-    char *arguments[] = {
-        "../bin/spliter",
-        "--routine",
-        "spliter.output_client",
-        nullptr
-    };
-    int my_argc = sizeof(arguments) / sizeof(arguments[0]) - 1;
-    app.initialize(my_argc, arguments);
-    output_client s(hs.new_split_stream(), 8002);
-    app.register_routine(&s);
-    s.set_engine(&engine);
+    netout<NTO_data_entry> no(hs.new_split_stream());
+    no.set_engine(&engine);
 
     engine.run();
-    app.run();
-    return 0; 
+    no.hpda_engine_complete();
+    return 0;
 }
--- doc_example/3.4/worker.cpp
+++ doc_example/3.4/worker.cpp
@@ -2,94 +2,7 @@
 #include <atomic>
 #include <cmath>
 #include <numeric>
-
-class extract_server 
-    : public hpda::extractor::internal::raw_data_impl<NTO_data_entry>,
-      public ff::net::routine 
-{
-public:
-    extract_server(int port)
-    : ff::net::routine("worker.extract_server"),
-      hpda::extractor::internal::raw_data_impl<NTO_data_entry>(),
-      done_transfer(false),
-      port(port)
-    {    }
-
-    virtual void initialize(ff::net::net_mode nm,
-                            const std::vector<std::string> &args)
-    {
-        pkghub.tcp_to_recv_pkg<NTP_data_entry>(std::bind(&extract_server::on_recv_data, this,
-                                                   std::placeholders::_1,
-                                                   std::placeholders::_2));
-        pkghub.tcp_to_recv_pkg<NTP_no_more_data_flag>(std::bind(&extract_server::on_recv_no_data, this,
-                                            std::placeholders::_1,
-                                            std::placeholders::_2));
-
-        netn = new ff::net::net_nervure(nm);
-        netn->add_pkg_hub(pkghub);
-        netn->add_tcp_server("127.0.0.1", port);
-        netn->get_event_handler()->listen<ff::net::event::tcp_get_connection>(
-            std::bind(&extract_server::on_conn_succ, this, std::placeholders::_1));
-        netn->get_event_handler()->listen<ff::net::event::tcp_lost_connection>(
-            std::bind(&extract_server::on_conn_lost, this, std::placeholders::_1, netn));
-    }
-
-    virtual void run() { 
-        std::thread monitor_thrd(std::bind(&extract_server::done_transfer_and_close_conn, this));
-        netn->run();
-        monitor_thrd.join();
-    }
-
-    bool process() override {
-        return done_transfer && hpda::extractor::internal::raw_data_impl<NTO_data_entry>::process();
-    }
-
-protected:
-    void on_recv_data(std::shared_ptr<NTP_data_entry> nt_data,
-                    ff::net::tcp_connection_base *client)
-    {
-        NTO_data_entry data;
-        data = nt_data-> template get<data_entry>();
-        add_data(data);
-        std::cout << "got data: " << data.get<phone_number>() << std::endl;
-    }
-
-    void on_recv_no_data(std::shared_ptr<NTP_no_more_data_flag> msg,
-                    ff::net::tcp_connection_base *client)
-    {
-        done_transfer.store(true);
-        std::cout << "got end flag" << std::endl;
-        std::shared_ptr<NTP_no_more_data_flag> pkg(new NTP_no_more_data_flag());
-        client->send(pkg);
-        std::cout<< "sent end flag" << std::endl;
-    }
-
-    void on_conn_succ(ff::net::tcp_connection_base *server)
-    {
-        ff::net::mout << "connect success, waiting for data ..." << std::endl;
-    }
-
-    void on_conn_lost(ff::net::tcp_connection_base *pConn,
-                    ff::net::net_nervure *netn)
-    {
-        ff::net::mout << "Client lost!" << std::endl;
-        netn->stop();
-    }
-
-    void done_transfer_and_close_conn() {
-        while(!done_transfer.load()) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-        // done transfer;
-        netn->stop();
-    } 
-
-protected:
-    int port;
-    std::atomic<bool> done_transfer;
-    ff::net::typed_pkg_hub pkghub;
-    ff::net::net_nervure *netn;
-};
+#include "netio.cpp"
 
 class groupby_and_sort
     : public hpda::processor::internal::processor_base<NTO_data_entry, NTO_grouped_data_entry>
@@ -97,47 +10,62 @@ class groupby_and_sort
 public:
     groupby_and_sort(hpda::internal::processor_with_output<NTO_data_entry> *upper_stream)
         : hpda::processor::internal::processor_base<NTO_data_entry, NTO_grouped_data_entry>(upper_stream)
-    {   }
+    {
+    }
 
-    bool process() override {
-        if(has_input_value()){
-            input_data.push_back(input_value());
+    bool process() override
+    {
+        if (has_input_value())
+        {
+            input_data.push_back(input_value().make_copy());
             consume_input_value();
             return false;
-        } else {
-            if (!grouped_and_sorted) {
+        }
+        else
+        {
+            if (!grouped_and_sorted)
+            {
                 do_groupby_and_sort();
             }
-            if (output_data.empty()) {
-            return false;
+            if (output_data.empty())
+            {
+                return false;
             }
             output_idx++;
-            if (output_idx >= static_cast<int>(output_data.size())) {
+            if (output_idx >= static_cast<int>(output_data.size()))
+            {
                 return false;
             }
             return true;
         }
     }
 
-    NTO_grouped_data_entry output_value() override {
+    NTO_grouped_data_entry output_value() override
+    {
         return output_data[output_idx];
-    } 
+    }
 
 private:
-
-    void do_groupby_and_sort() {
-        std::unordered_map<ff::util::internal::nt_traits<phone_number>::type, 
-                std::vector<NTO_loc_info>> groupMap;
-        for (auto& entry: input_data) {
+    void do_groupby_and_sort()
+    {
+        std::unordered_map<ff::util::internal::nt_traits<phone_number>::type,
+                           std::vector<NTO_loc_info>>
+            groupMap;
+        for (auto &entry : input_data)
+        {
             auto key = entry.get<phone_number>();
-            if (groupMap.count(key) > 0) {
+            if (groupMap.count(key) > 0)
+            {
                 groupMap[key].push_back(entry.get<loc_info>());
-            } else {
+            }
+            else
+            {
                 groupMap[key] = std::vector<NTO_loc_info>{entry.get<loc_info>()};
             }
         }
 
-        for (auto entry : groupMap) {
+        for (auto entry : groupMap)
+        {
             // sort the grouped loc_info entry
             auto sorting_v = entry.second;
             quicksort(sorting_v);
@@ -151,12 +79,15 @@ private:
         grouped_and_sorted = true;
     }
 
-    int partition(std::vector<NTO_loc_info>& arr, int low, int high) {
+    int partition(std::vector<NTO_loc_info> &arr, int low, int high)
+    {
         NTO_loc_info pivot = arr[high];
         int i = low - 1;
 
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j].get<timestamp>() <= pivot.get<timestamp>()) {
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (arr[j].get<timestamp>() <= pivot.get<timestamp>())
+            {
                 i++;
                 NTO_loc_info tmp = arr[i].make_copy();
                 arr[i] = arr[j];
@@ -170,15 +101,18 @@ private:
         return i + 1;
     }
 
-    void quicksort(std::vector<NTO_loc_info>& arr, int low, int high) {
-        if (low < high) {
+    void quicksort(std::vector<NTO_loc_info> &arr, int low, int high)
+    {
+        if (low < high)
+        {
             int pi = partition(arr, low, high);
             quicksort(arr, low, pi - 1);
             quicksort(arr, pi + 1, high);
         }
     }
 
-    void quicksort(std::vector<NTO_loc_info>& arr) {
+    void quicksort(std::vector<NTO_loc_info> &arr)
+    {
         quicksort(arr, 0, arr.size() - 1);
     }
 
@@ -192,9 +126,10 @@ class cal_distance
     : public hpda::processor::internal::processor_base<NTO_grouped_data_entry, NTO_distance_entry>
 {
 public:
-    cal_distance (hpda::internal::processor_with_output<NTO_grouped_data_entry> *upper_stream)
+    cal_distance(hpda::internal::processor_with_output<NTO_grouped_data_entry> *upper_stream)
         : hpda::processor::internal::processor_base<NTO_grouped_data_entry, NTO_distance_entry>(upper_stream)
-    {   }
+    {
+    }
 
     bool process() override
     {
@@ -208,7 +143,8 @@ public:
         return true;
     }
 
-    NTO_distance_entry output_value() override {
+    NTO_distance_entry output_value() override
+    {
         return tmp;
     }
 
@@ -216,12 +152,14 @@ private:
     NTO_distance_entry tmp;
 
     // Function to convert degrees to radians
-    static double toRadians(double degrees) {
+    static double toRadians(double degrees)
+    {
         return degrees * M_PI / 180.0;
     }
 
     // Function to calculate the distance between two points given their latitude and longitude
-    static double calculateDistance(NTO_loc_info loc1, NTO_loc_info loc2) {
+    static double calculateDistance(NTO_loc_info loc1, NTO_loc_info loc2)
+    {
         double earthRadiusKm = 6371.0; // Radius of the Earth in kilometers
 
         // Convert latitude and longitude to radians
@@ -236,19 +174,22 @@ private:
 
         // Calculate the distance using the Haversine formula
         double a = sin(latDiff / 2.0) * sin(latDiff / 2.0) +
-                cos(lat1Rad) * cos(lat2Rad) *
-                sin(lonDiff / 2.0) * sin(lonDiff / 2.0);
+                   cos(lat1Rad) * cos(lat2Rad) *
+                       sin(lonDiff / 2.0) * sin(lonDiff / 2.0);
         double c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a));
         double distance = earthRadiusKm * c;
 
         return distance;
     }
 
-    double calculateDistance(std::vector<NTO_loc_info> v) {
+    double calculateDistance(std::vector<NTO_loc_info> v)
+    {
         double dist = 0;
-        if (v.size() > 1) {
-            for(int i = 1; i < v.size(); i++) {
-                dist += calculateDistance(v[i-1], v[i]);
+        if (v.size() > 1)
+        {
+            for (int i = 1; i < v.size(); i++)
+            {
+                dist += calculateDistance(v[i - 1], v[i]);
             }
         }
         return dist;
@@ -262,53 +203,67 @@ public:
     max_n(hpda::internal::processor_with_output<NTO_distance_entry> *upper_stream, uint size_limit)
         : hpda::processor::internal::processor_base<NTO_distance_entry, NTO_distance_entry>(upper_stream),
           size_limit(size_limit)
-    {   }
+    {
+    }
 
-    bool process() override {
-        if(has_input_value()){
+    bool process() override
+    {
+        if (has_input_value())
+        {
             insert(max_n_data, input_value());
-            if(max_n_data.size() > size_limit) {
+            if (max_n_data.size() > size_limit)
+            {
                 max_n_data.pop_back();
             }
 
             consume_input_value();
             return false;
         }
-        if(max_n_data.empty()) {
+        if (max_n_data.empty())
+        {
             return false;
         }
         output_idx++;
-        if (output_idx >= max_n_data.size()) {
+        if (output_idx >= max_n_data.size())
+        {
             return false;
         }
         return true;
     }
 
-    NTO_distance_entry output_value() override {
+    NTO_distance_entry output_value() override
+    {
         return max_n_data[output_idx];
-    } 
+    }
 
 private:
-    void insert(std::vector<NTO_distance_entry>& l, NTO_distance_entry new_item) {
+    void insert(std::vector<NTO_distance_entry> &l, NTO_distance_entry new_item)
+    {
         l.push_back(new_item.make_copy()); // a place holder
-        int i = l.size()-2;
-        while(i>=0) {
-            if(l[i].get<distance>() < new_item.get<distance>()){
-                l[i+1] = l[i].make_copy();
-            } else {
+        int i = l.size() - 2;
+        while (i >= 0)
+        {
+            if (l[i].get<distance>() < new_item.get<distance>())
+            {
+                l[i + 1] = l[i].make_copy();
+            }
+            else
+            {
                 break;
             }
             i--;
         }
-        i++; //the place to actually insert
-        if(i>=0){
+        i++; // the place to actually insert
+        if (i >= 0)
+        {
             l[i] = new_item;
         }
         return;
     }
 
-    static bool compare_by_distance(const NTO_distance_entry& a, const NTO_distance_entry& b) {
-        return a.get<distance>() > b.get<distance>();    // descending order
+    static bool compare_by_distance(const NTO_distance_entry &a, const NTO_distance_entry &b)
+    {
+        return a.get<distance>() > b.get<distance>(); // descending order
     }
 
     std::vector<NTO_distance_entry> max_n_data;
@@ -316,36 +271,30 @@ private:
     uint size_limit;
 };
 
-
 int main(int argc, char *argv[])
 {
     hpda::engine engine;
 
+    netin<NTO_data_entry> ni;
+    ni.set_engine(&engine);
 
-    ff::net::application app("worker");
-    char *arguments[] = {
-        "../bin/worker",
-        "--routine",
-        "worker.extract_server",
-        nullptr
-    };
-    int my_argc = sizeof(arguments) / sizeof(arguments[0]) - 1;
-    app.initialize(my_argc, arguments);
-    extract_server es(8002);
-    app.register_routine(&es);
-    es.set_engine(&engine);
+    groupby_and_sort gs(&ni);
+    gs.set_engine(&engine);
 
-    groupby_and_sort gs(&es);
     cal_distance cd(&gs);
-    max_n mn(&cd, 3);
+    cd.set_engine(&engine);
 
-    hpda::output::internal::memory_output_impl<NTO_distance_entry> checker( &mn );
-    
-    app.run();
+    max_n mn(&cd, 5);
+    mn.set_engine(&engine);
+
+    hpda::output::internal::memory_output_impl<NTO_distance_entry> checker(&mn);
+    checker.set_engine(&engine);
+
     engine.run();
 
-    std::cout << checker.values().size() << std::endl;
-    for (auto v : checker.values()) {
+    std::cout << "size " << checker.values().size() << std::endl;
+    for (auto v : checker.values())
+    {
         std::cout << v.get<phone_number>() << "|";
         std::cout << v.get<distance>() << std::endl;
         /*
